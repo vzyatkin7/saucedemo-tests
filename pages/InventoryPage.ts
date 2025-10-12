2b67e69bf61c3dd0375c708e1bc88927b07cdbd4
Egor Hor <vzyatkin7@gmail.com>
2025-10-12 16:00:46 +0700

    delete method

--- pages/InventoryPage.ts
+++ pages/InventoryPage.ts
@@ -20,13 +20,6 @@ export class InventoryPage {
     await expect(this.page.locator(inventoryPageLocators.shoppingCartBadge)).toHaveText('1');
   }
 
-  // проверка, что 1 товар добавлен в корзину
-  async verifyProductInCart(productName: string) {
-    const removeFromCartButton = inventoryPageLocators.removeFromCartButton(this.page, productName);
-    await removeFromCartButton.waitFor({ state: 'visible' });
-    await expect(this.page.locator(inventoryPageLocators.shoppingCartBadge)).toHaveText('1');
-  }
-
   // удаление товара из корзины по имени продукта
   async removeProductFromCart(productName: string) {
     const removeFromCartButton = inventoryPageLocators.removeFromCartButton(this.page, productName);
